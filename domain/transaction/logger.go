6b6ffb08ffd63850cd8cd1e563e45ab9f3810e10
wisoot <wisoot@gmail.com>
2018-01-23 20:55:58 +1100

    Implement transaction logger

--- domain/transaction/logger.go
+++ domain/transaction/logger.go
@@ -3,7 +3,13 @@ package transaction
 import "edb/entities/transaction"
 
 type Logger struct {
+	Repository LoggerRepository
 }
 
-func (logger *Logger) LogTransaction(log transaction.Log) {
+type LoggerRepository interface {
+	Create(log transaction.Log) error
+}
+
+func (logger *Logger) LogTransaction(log transaction.Log) error {
+	return logger.Repository.Create(log)
 }
